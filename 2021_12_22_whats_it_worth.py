41b43f888f3b83a661d6e639e09bc51eae3ce293
Arjun Krishna <45014214+arjun-krishna1@users.noreply.github.com>
2021-12-22 12:12:32 -0500

    Update 2021_12_22_whats_it_worth.py

--- 2021_12_22_whats_it_worth.py
+++ 2021_12_22_whats_it_worth.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-# row 1, col1 , col2, , col3, col4
+'''
+https://nrich.maths.org/1053
+
+I turned this into a Linear Algebra problem
+I made each shape a variable
+They were added together to give the resulting value for each row and column
+  i.e. a system of linear equations
+  
+There are 5 variables, the value of each shape and the question mark
+
+I needed 5 equations, I chose row 1 and all of the columns
+
+A*res = B
+
+We know A and B so we can use numpy to solve for res using the
+numpy linear algebra solver
+'''
+
+# the constants for each equation
 B = np.array([28, 0, 30, 23, 22])
 
+# the coefficient matrix for the system of equations
 A = np.array([
   [2, 2, 0,0,0],
   [1, 0, 1, 2, -1],
@@ -11,11 +30,14 @@ A = np.array([
   [1, 1, 0, 2, 0]
 ])
 
+# solving for the result
 res = np.linalg.solve(A, B)
 
+# displaying the results
 print(" tr, sq, he, ci, ?")
 print(res)
 
+# verifying res:
 B_expected = np.matmul(A, res)
 
 print("\nshould be the same")
